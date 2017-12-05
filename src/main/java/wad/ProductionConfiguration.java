9af90715dd2eadb0e3abeb859272f9585771ff03
palchyk <palchyk@github.com>
2017-12-05 02:18:56 +0200

    mm

--- src/main/java/wad/ProductionConfiguration.java
+++ src/main/java/wad/ProductionConfiguration.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package wad;
 
-/**
- *
- * @author y50u
- */
+import javax.sql.DataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("production")
+@Configuration
 public class ProductionConfiguration {
-    
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        System.out.println("Hello world!");
+        return DataSourceBuilder.create().build();
+    }
 }
