3565c9a7648afa31ba18e84000756c9b4f6a8974
palchyk <palchyk@github.com>
2017-12-06 00:04:50 +0200

    eka

--- src/main/java/wad/controller/AliveController.java
+++ src/main/java/wad/controller/AliveController.java
@@ -3,7 +3,9 @@ package wad.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import wad.domain.Item;
@@ -25,6 +27,11 @@ public class AliveController {
     public String create(@RequestParam String name) {
         Item i = new Item(name);
         itemRepository.save(i);
+        return "redirect:/";
+    }
+     @DeleteMapping("/remove")
+    public String remove(Model model) {
+        itemRepository.deleteAll();
         return "redirect:/";
     }
 }
